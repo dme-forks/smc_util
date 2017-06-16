0b7c84e326a4b30d5194c29ad5e7826d0d0c5551
Florian Echtler <floe@butterbrot.org>
2017-06-16 09:06:43 +0200

    just use SUID binary

--- tdm_off.sh
+++ tdm_off.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-./SmcDumpKey MVHR 0
+SmcDumpKey MVHR 0
 sleep 1
-./SmcDumpKey MVMR 2
+SmcDumpKey MVMR 2
 sleep 2
 DISPLAY=:0.0 xrandr --output eDP --auto
--- tdm_on.sh
+++ tdm_on.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-./SmcDumpKey MVHR 1
+SmcDumpKey MVHR 1
 sleep 1
-./SmcDumpKey MVMR 2
+SmcDumpKey MVMR 2
 sleep 2
 DISPLAY=:0.0 xrandr --output eDP --off
